3b147cfded523db569cec1fc76b7b058e1802790
Okugata Fahmi <otasrit@gmail.com>
2020-02-11 12:04:26 +0700

    Bisa menang

--- java/src/main/java/za/co/entelect/challenge/Bot.java
+++ java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -51,48 +51,77 @@ public class Bot {
 
         // kalau musuh ada iron curtain, kita membuat iron curtain juga
         if (opponent.isIronCurtainActive && gameDetails.round>0){
-            if (myself.energy>=gameDetails.ironCurtainStats.price){
+            if (canAffordActivatingShield()){
                 command = "7,7,5";
                 return command;
             }
             // jika energi kurang, maka tunggu energi cukup di turn selanjutnya
             else return NOTHING_COMMAND;
         }
-        else if (myself.energy>=gameDetails.ironCurtainStats.price && myself.ironCurtainAvailable){
+        else if (canAffordActivatingShield() && myself.ironCurtainAvailable){
             return "7,7,5";
         }
 
         // kalau musuh ada tesla, kita buat iron curtain
-        List<Building> buildingPlayerBtesla = getPlayerThingList(buildings,b->b.isEqual(BuildingType.TESLA, PlayerType.B));
+        List<Building> buildingPlayerBtesla = getPlayerThingList(buildings,b->b.isEqual(BuildingType.TESLA, PlayerType.B))
+                                             .stream().filter(b->b.constructionTimeLeft<=gameDetails.ironCurtainStats.activeRounds)
+                                             .collect(Collectors.toList());
         if (buildingPlayerBtesla.size()>0){
-            if (myself.energy>=gameDetails.ironCurtainStats.price){
+            if (canAffordActivatingShield()){
                 command = "7,7,5";
                 return command;
             }   
             else return NOTHING_COMMAND;
         }
 
-        // membangun tesla bila sudah punya banyak energy
-        if (myself.energy-100>=gameDetails.buildingsStats.get(BuildingType.TESLA).price){
-            int y=99,maxEnemyAttack=-1;
-            for (int i = 1; i<gameHeight-1; ++i){
-                final int y_temp = i;
-                int enemyAttackOnRow = getPlayerThingList(buildings, b-> b.isEqual(y_temp, BuildingType.ATTACK, PlayerType.B)).size();
-                int myBuildingsOnRow = getPlayerThingList(buildings, b-> b.isEqual(y_temp, PlayerType.A)).size();
-                if (enemyAttackOnRow>maxEnemyAttack && myBuildingsOnRow<gameWidth/2-1){
-                    maxEnemyAttack = enemyAttackOnRow;
-                    y = i;
-                }
-            }
+        // membangun defensive wall jika kita punya tesla
+        List<Building> myTesla = getPlayerThingList(buildings, b->b.isEqual(BuildingType.TESLA,PlayerType.A));
+        if (myTesla.size()>0 && canAffordBuilding(BuildingType.DEFENSE)){
+            for (int i=0; i<myTesla.size(); ++i){
+                final int y = myTesla.get(i).getY();
+                int myDefenseOnRow = getPlayerThingList(buildings, b->b.isEqual(y, BuildingType.DEFENSE, PlayerType.A)).size();
 
-            for (int j = 2; j<gameWidth/2-2; ++j){
-                if (isCellEmpty(j, y)){
-                    return buildCommand(j, y, BuildingType.TESLA);
+                if (myDefenseOnRow==0){
+                    return buildFromFrontAtRow(BuildingType.DEFENSE, y);
                 }
             }
         }
+        
+        // membangun tesla bila sudah punya banyak energy. Bila sudah bikin 1, bangun defense saja
+        if (myself.energy>=gameDetails.buildingsStats.get(BuildingType.TESLA).price){
+            if (myTesla.size()==0){
+                int y=99,maxEnemyAttack=-1;
+                for (int i = 1; i<gameHeight-1; ++i){
+                    final int y_temp = i;
+                    int enemyAttackOnRow = getPlayerThingList(buildings, b-> b.isEqual(y_temp, BuildingType.ATTACK, PlayerType.B)).size();
+                    int myBuildingsOnRow = getPlayerThingList(buildings, b-> b.isEqual(y_temp, PlayerType.A)).size();
+                    if (enemyAttackOnRow>maxEnemyAttack && myBuildingsOnRow<gameWidth/2-1){
+                        maxEnemyAttack = enemyAttackOnRow;
+                        y = i;
+                    }
+                }
 
-        // langsung bangun attack ketika sudah punya cukup energy
+                for (int j = 3; j<gameWidth/2-2; ++j){
+                    if (isCellEmpty(j, y)){
+                        return buildCommand(j, y, BuildingType.TESLA);
+                    }
+                }
+            }
+            else{ // bangun defense saja, dan pasti mampu membangun karena energy ada banyak
+                int maxEnemyAttack = -1, y = 99;
+                for (int i=0; i<gameHeight; ++i){
+                    final int y_temp = i;
+                    int enemyAttackOnRow = getPlayerThingList(buildings, b->b.isEqual(y_temp, BuildingType.ATTACK, PlayerType.B)).size();
+                    if (enemyAttackOnRow>maxEnemyAttack && isCellEmpty((gameWidth/2-1), i)){
+                        maxEnemyAttack = enemyAttackOnRow;
+                        y = i;
+                    }
+                }
+                return buildFromFrontAtRow(BuildingType.DEFENSE, y);
+            }
+        }
+
+        // langsung bangun attack di baris yg bangunan musuh paling sedikit ketika sudah punya cukup energy
         if (canAffordBuilding(BuildingType.ATTACK)){
             int minEnemyBuilding = 99, y = 99;
             for (int i=0; i<gameHeight; ++i){
@@ -126,10 +155,10 @@ public class Bot {
         for (int i=0; i<gameHeight; ++i) {
             final int y = i;
             List<Building> friendBuildingEnergy = getPlayerThingList(buildings, b->b.isEqual(y, BuildingType.ENERGY, PlayerType.A));
-            List<Building> friendBuildingDefense = getPlayerThingList(buildings, b->b.isEqual(y, BuildingType.DEFENSE, PlayerType.A));
+            List<Building> friendBuilding = getPlayerThingList(buildings, b->b.isEqual(y, PlayerType.A));
             List<Building> opponentBuildingAttack = getPlayerThingList(buildings, b->b.isEqual(y, BuildingType.ATTACK, PlayerType.B));
 
-            if (friendBuildingEnergy.size()==0 && (opponentBuildingAttack.size()==0 || friendBuildingDefense.size()>0)) {
+            if (friendBuildingEnergy.size()==0 && (opponentBuildingAttack.size()==0 || friendBuilding.size()>0)) {
                 if (canAffordBuilding(BuildingType.ENERGY)) {
                     command = buildFromBacktAtRow(BuildingType.ENERGY,i);
                     if (!command.equals("")){
@@ -137,7 +166,7 @@ public class Bot {
                     }
                 }
             }
-            else if (friendBuildingEnergy.size()==0 && friendBuildingDefense.size()==0){
+            else if (friendBuildingEnergy.size()==0 && friendBuilding.size()==0){
                 if (canAffordBuilding(BuildingType.DEFENSE)) {
                     command = buildFromBacktAtRow(BuildingType.DEFENSE,i);
                     if (!command.equals("")){
@@ -166,6 +195,11 @@ public class Bot {
     // mengembalikan true apabila kita dapat membangun suatu gedung
     private boolean canAffordBuilding(BuildingType buildingType) {
         return myself.energy >= gameDetails.buildingsStats.get(buildingType).price;
+    }
+
+    // mengembalikan true apabila kita dapat mengaktifkan ironcurtain
+    private boolean canAffordActivatingShield() {
+        return myself.energy >= gameDetails.ironCurtainStats.price;
     }
 
     // mengembalikan string command untuk membangung bangunan
